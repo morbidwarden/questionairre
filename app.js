2df5d777093721393a72052aec45c84c25a953d0
Aashutosh Singh <ashutosh360a@gmail.com>
2024-05-18 19:39:41 +0530

    commiting changes

---
+++ app.js
@@ -0,0 +1,64 @@
+var express=require("express")
+var bodyParser=require("body-parser")
+var mongoose=require("mongoose")
+
+const app=express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+
+
+
+const {MONGO_IP, MONGO_PORT, MONGO_USER, MONGO_PASSWORD} = require("./config/config");
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+mongoose.connect(
+    mongoURL)
+    .then(() => console.log("MongoDB started..."))
+    .catch((e) => {console.log("Error :",e)});
+
+// mongoose.connect('mongodb://127.0.0.1:27017/mydatabase')
+
+
+var db=mongoose.connection
+db.on('error',()=> console.log("Error in Connecting to Database"))
+db.once('open',()=> console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res) => {
+    var name= req.body.name
+    var age=req.body.age
+    var email=req.body.email
+    var phno=req.body.phno
+    var gender=req.body.gender
+    var password=req.body.password
+
+    var data={
+        "name":name,
+        "age":age,
+        "email":email,
+        "phno":phno,
+        "gender":gender,
+        "password":password
+    }
+    db.collection('users').insertOne(data,(err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Succesfully")
+    })
+    return res.redirect('index.html')
+})
+
+app.get("/",(req,res) => {
+    res.set({
+        "Allow-acces-Allow-Origin":'*'
+    })
+    return res.redirect('index.html')
+}).listen(3000);
+
+console.log("Listening on port 3000")
